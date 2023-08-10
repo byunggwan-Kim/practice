8048677e281a3f3195975086ad59a707454e02a6
SHIN1501020 <shin8hh@gmail.com>
2023-08-10 10:23:10 +0900

    리뷰 수정/삭제 기능 추가

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 from pymongo import MongoClient
 from flask import Flask, render_template, request, jsonify
+from bson.objectid import ObjectId
 
 import certifi
 # pymongo 에러 해결을 위해 import
@@ -135,11 +136,73 @@ def home():
 #     all_moives = list(db.movies2.find({},{'_id':False}))
 #     return jsonify({'result': all_moives })
 
+#* 리뷰 조회
 @app.route("/api/reviews", methods=["GET"])
 def reviews_get():
-    all_reviews = list(db.review.find({}, {'_id': False}))
-    print(all_reviews)
-    return jsonify({'reviews': all_reviews})
+    all_reviews = list(db.review.find({}))
+    for a in all_reviews:
+            a['_id'] = str(a['_id'])#ObjectId Class을 str로 변경
+    return jsonify({'reviews': all_reviews })
+
+#* 리뷰 삭제
+@app.route("/api/reviews/delete", methods=["POST"])
+def reviews_delete():
+    #현재 로그인 사용자 토근값
+    review_token = request.form['review_token']#현재 사용자 토근값 전달
+    token_user = jwt.decode(review_token, SECRET_KEY, algorithms=['HS256'])#토큰 디코드
+    user = token_user['user']#현재 사용자의 username
+
+    #review table '_id' 값
+    review_id = request.form['review_id']
+
+    #'_id' 값과  'wirte_usr'(현재 사용자 username)이 같다면 delete
+    result = db.review.delete_one({
+            '_id':ObjectId(review_id),
+            'write_user':user})
+    
+    if(result.deleted_count == 1) :
+        return jsonify({'reviews': "삭제 완료!" })
+    else :
+        return jsonify({'reviews': "삭제 권한이 없습니다." })
+
+#* 리뷰 id별로 수정 페이지 이동(flask 동적 라우팅)
+@app.route("/api/reviews/<id>", methods=["GET"])
+def reviews_update_page(id):
+    return render_template('review_form.html', id=id)
+
+#* 리뷰 id별로 데이터 조회
+@app.route("/api/reviews/<id>/update_form", methods=["GET"])
+def reviews_update_form(id):
+    review = db.review.find_one({'_id':ObjectId(id)})
+    review['_id'] = str(review['_id'])
+    return jsonify({'review': review })
+
+#* 리뷰 데이터 수정(update)
+@app.route("/api/reviews/<id>/update", methods=["POST"])
+def reviews_update(id):
+    #현재 로그인 사용자 토큰값
+    review_token = request.form['review_token']
+    token_user = jwt.decode(review_token, SECRET_KEY, algorithms=['HS256'])
+    user = token_user['user']
+    
+    review_title = request.form['review_title']
+    review_author = request.form['review_author']
+    review_description = request.form['review_description']
+    review_star = request.form['review_star']
+    review_comment = request.form['review_comment']
+
+    #'_id' 값과  'wirte_usr'(현재 사용자 username)이 같다면 update
+    result = db.review.update_one({'_id':ObjectId(id), 'write_user':user}, {'$set' : {'title' : review_title,
+        'author' : review_author,
+        'description' : review_description,
+        'star' : review_star,
+        'comment' : review_comment}                                           
+    })
+    
+    if(result.matched_count == 1):
+         return jsonify({'review': "수정 완료!" })
+    else :
+        return jsonify({'review': "수정 권한이 없습니다!" })
 
 
 if __name__ == '__main__':
